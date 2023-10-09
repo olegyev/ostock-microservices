048e981c4d69f991b379253abd7a5aad1326df96
olegyev <o.yevstratyev@gmail.com>
2023-10-10 00:22:12 +0200

    Configure organization-service Keycloak OAuth 2.0 Auth Server + Spring Boot 3 Resource Server

---
+++ organization-service/src/main/java/com/optimagrowth/organization/config/KeycloakSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.optimagrowth.organization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+public class KeycloakSecurityConfig {
+
+    /**
+     * @see config/organization-service.yml#security.oauth2.resourceserver.jwt
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/v1/organization/**").hasRole("OSTOCK_ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2Configurer -> oauth2Configurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+                    Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+                    Collection<String> roles = realmAccess.get("roles");
+                    var grantedAuthorities = roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                            .toList();
+                    return new JwtAuthenticationToken(jwt, grantedAuthorities);
+                })))
+        ;
+
+        return httpSecurity.build();
+    }
+}
